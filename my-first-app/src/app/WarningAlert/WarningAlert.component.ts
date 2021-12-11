b03f9934354f8f9041c8ff879fa8210c8ff06ca0
Bezdna287 <bezdnatm@gmail.com>
2021-12-11 21:53:25 +0100

    components add

---
+++ my-first-app/src/app/WarningAlert/WarningAlert.component.ts
@@ -0,0 +1,11 @@
+import { templateJitUrl } from "@angular/compiler";
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-WarningAlert',
+    templateUrl: '/WarningAlert.component.html',
+    styleUrls: ['./WarningAlert.component.css']
+})
+export class WarningAlertComponent {
+
+}
--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent} from './SuccessAlert/SuccessAlert.component';
+import { WarningAlertComponent} from './WarningAlert/WarningAlert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,  FormsModule
